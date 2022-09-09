8798d54207a04bab547f0848cdb1c3436694826b
kzish <kudzai@globelise.com>
2022-09-09 14:25:20 +0200

    init commit

---
+++ carSite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using carSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace carSite.Controllers
+{
+    [Route("")]
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Advert")]
+        public IActionResult Advert()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
